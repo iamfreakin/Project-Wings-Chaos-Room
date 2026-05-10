6cc2746ef1a322bf8d7fe404c6f5188e07c2c43a
qjaqj <qjaqjaqja1877@gmail.com>
2026-05-10 15:09:03 +0900

    INIT

---
+++ ProjectWings/Source/ProjectWings/Public/WingsTest.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECTWINGS_API WingsTest
+{
+public:
+	WingsTest();
+	~WingsTest();
+};
